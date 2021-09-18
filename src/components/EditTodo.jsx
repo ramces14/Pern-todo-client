84f0ac2e33ed1b9623ea4694d48481f7b8c4f84c
Ramces Gil <ramces@Ramcess-MacBook-Air.local>
2021-09-18 17:36:31 -0400

    changes made

---
+++ src/components/EditTodo.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+function EditTodo({ todo }) {
+	const [description, setDescription] = useState(todo.description);
+
+	const updateDescription = async (e) => {
+		e.preventDefault();
+		try {
+			const body = { description };
+			const editedTodo = await fetch(`/todos/${todo.todo_id}`, {
+				method: "PUT",
+				headers: { "Content-type": "application/json" },
+				body: JSON.stringify(body),
+			});
+			window.location = "/";
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
+	return (
+		<div className="container">
+			<button
+				type="button"
+				className="btn btn-warning"
+				data-toggle="modal"
+				data-target={`#id${todo.todo_id}`}
+			>
+				Edit
+			</button>
+
+			<div className="modal" id={`id${todo.todo_id}`}>
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h4 className="modal-title">Edit Todo</h4>
+							<button
+								type="button"
+								className="close"
+								data-dismiss="modal"
+								onClick={() => setDescription(todo.description)}
+							>
+								X
+							</button>
+						</div>
+
+						<div className="modal-body">
+							<input
+								type="text"
+								className="form-control"
+								value={description}
+								onChange={(e) => setDescription(e.target.value)}
+							/>
+						</div>
+
+						<div className="modal-footer">
+							<button
+								type="button"
+								className="btn btn-warning"
+								data-dismiss="modal"
+								onClick={(e) => updateDescription(e)}
+							>
+								Edit
+							</button>
+							<button
+								type="button"
+								className="btn btn-danger"
+								data-dismiss="modal"
+								onClick={() => setDescription(todo.description)}
+							>
+								Close
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default EditTodo;
